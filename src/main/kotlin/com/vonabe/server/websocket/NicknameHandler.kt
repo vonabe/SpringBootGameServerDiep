9b5b5f66305d6cbeaa88e7d3dcb8334f6bc2f65a
vonabe <vonabe.dev@gmail.com>
2024-10-07 21:30:00 +0300

    ssl enable

--- src/main/kotlin/com/vonabe/server/websocket/NicknameHandler.kt
+++ src/main/kotlin/com/vonabe/server/websocket/NicknameHandler.kt
@@ -4,24 +4,10 @@ import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
 
 @Component
-class NicknameHandler(private val resourceLoader: ResourceLoader) {
+class NicknameHandler(resourceLoader: ResourceLoader) {
 
-    private var listNickname: MutableList<String>? = null
-
-    init {
-        val resource = resourceLoader.getResource("classpath:nicknames.csv")
-        val inputStream = resource.inputStream
-
-        // Now you can read from the input stream as needed
-        // For example, if you want to read it as a string
-        val nicknames = inputStream.bufferedReader().use { it.readText() }
-        listNickname = nicknames.lines().toMutableList()
-
-//        ResourceUtils.getFile("classpath:nicknames.csv").let {
-//            listNickname = Files.readAllLines(it.toPath())
-        //            listNickname = Files.readAllLines(ClassPathResource("/nicknames.csv").file.toPath())
-//        }
-    }
+    private var listNickname: List<String> =
+        resourceLoader.getResource("classpath:nicknames.csv").inputStream.bufferedReader().readLines()
 
     fun randomNickname(): String {
         return listNickname?.random() ?: "Empty"
