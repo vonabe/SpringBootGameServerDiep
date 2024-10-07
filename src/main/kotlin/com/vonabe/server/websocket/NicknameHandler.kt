11c6dc5e9cf1c83bb699153cdc5fa0a1a111e7b7
vonabe <vonabe.dev@gmail.com>
2024-10-07 17:39:36 +0300

    nickname handler path edit

--- src/main/kotlin/com/vonabe/server/websocket/NicknameHandler.kt
+++ src/main/kotlin/com/vonabe/server/websocket/NicknameHandler.kt
@@ -1,7 +1,7 @@
 package com.vonabe.server.websocket
 
-import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
+import org.springframework.util.ResourceUtils
 import java.nio.file.Files
 
 @Component
@@ -10,7 +10,10 @@ class NicknameHandler {
     private var listNickname: MutableList<String>? = null
 
     init {
-        listNickname = Files.readAllLines(ClassPathResource("/nicknames.csv").file.toPath())
+        ResourceUtils.getFile("classpath:nicknames.csv").let {
+            listNickname = Files.readAllLines(it.toPath())
+    //            listNickname = Files.readAllLines(ClassPathResource("/nicknames.csv").file.toPath())
+        }
     }
 
     fun randomNickname(): String {
